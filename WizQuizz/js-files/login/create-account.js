97f2326855abeca5b3126cfb16b2ced9bed33682
rgarciapedroza <rosmarygp11@gmail.com>
2024-03-12 22:45:14 +0000

    Create account via html connected with firebase database

---
+++ WizQuizz/js-files/login/create-account.js
@@ -0,0 +1,23 @@
+import { setUserData } from "../common/backend-functions.js";
+
+document.addEventListener('DOMContentLoaded', async function() {
+    var createAccountButton = document.querySelector(".create-account-button");
+
+    
+    createAccountButton.addEventListener('click', function() {
+        var nickname = document.getElementById("nickname-input").value.toString();
+        var email = document.getElementById('email-input').value.toString();
+        var password =  document.getElementById('password-input').value.toString();
+        var description = "noDescription";
+        var image = "noImage";
+        var confirmPassword = document.getElementById('confirm-password-input').value.toString();
+        if (password === confirmPassword){
+            setUserData(email, nickname, description, image, password);
+            console.log("Cuenta creada con éxito.")
+        } else {
+            console.log("Las contraseñas no coinciden.");
+        }
+        
+    });
+    
+})
