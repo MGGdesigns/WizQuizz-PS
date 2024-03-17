2a3f3222e93267aee12bf185437ece6297d4018e
rgarciapedroza <rosmarygp11@gmail.com>
2024-03-17 19:55:54 +0000

    test user created

--- WizQuizz/js-files/login/create-account.js
+++ WizQuizz/js-files/login/create-account.js
@@ -1,16 +1,25 @@
-import { getAllUsers, createUser } from "../../../../paraCopiar/WizQuizz-PS/WizQuizz/js-files/common/backend-functions.js";
+import { getAllUsers, createUser } from "../../../../WizQuizz-PS/WizQuizz/js-files/common/backend-functions.js";
+
+
 
 document.addEventListener('DOMContentLoaded', async function() {
     var createAccountButton = document.querySelector(".create-account-button");
    
-    createAccountButton.addEventListener('click', async function() {
+    createAccountButton.addEventListener('click', async function(e) {
+        event.preventDefault(e);
         var username = document.getElementById("nickname-input").value.trim();
         var email = document.getElementById('email-input').value.trim();
         var password = document.getElementById('password-input').value.trim();
-        var description = "noDescription";
+        var description = "In the annals of magic, there exists a whispered legend of a wizard unparalleled: Zephyrion, the Arcane Sovereign. With robes swirling like tempests, he wields spells of unfathomable power. From ancient tomes, he conjures storms of brilliance, shaping reality itself. His name resonates through time as a beacon of mystic mastery.";
         var confirmPassword = document.getElementById('confirm-password-input').value.trim();
-        
+       
+        if (!username || !email || !password || !confirmPassword) {
+            alert("Por favor, completa todos los campos.");
+            return;
+        }
         if (password === confirmPassword) {
+            
+            
             try {
                 // Obtener todos los usuarios
                 const users = await getAllUsers();
@@ -19,19 +28,29 @@ document.addEventListener('DOMContentLoaded', async function() {
                 // Verificar si el usuario ya existe
                 const existingUser = Object.values(users).find(user => user.username === username);
                 console.log("Usuario existente:", existingUser);
-                
+
                 if (existingUser) {
-                    console.log("Usuario con cuenta ya registrada");
+                    alert("Usuario con cuenta ya registrada");
                 } else {
                     // Crear la cuenta
-                    await createUser(username, email, password, description, "", "", "0");
-                    console.log("La cuenta ha sido creada con éxito.");
-                }
-            } catch (error) {
-                console.error("Error al obtener usuarios:", error);
+                    const fecha = new Date();
+                    let dia = fecha.getDate();
+                    let mes = fecha.getMonth() + 1;
+                    let año = fecha.getFullYear();
+                    let fechaCompleta = String(dia) + "-" + String(mes) + "-" + String(año);
+                    let nameuser = sessionStorage.setItem("actualUser", username);
+                    console.log(nameuser.username);
+                    await createUser(nameuser, email, password, description, "../../website-images/common/insert-image.png", fechaCompleta, "0");
+                    window.location.href = '../../src/login/user-profile.html'; 
+                    alert("La cuenta ha sido creada con éxito.");
+
+                    
+            }}catch(error) {
+                console.log("error");
+               // console.error("Error al obtener usuarios:", error);
             }
         } else {
-            console.log("Las contraseñas no coinciden.");
+            alert("Las contraseñas no coinciden.");
         }
     });
 });
--- WizQuizz/js-files/login/login.js
+++ WizQuizz/js-files/login/login.js
@@ -1,17 +1,33 @@
-import { setUserData } from "../common/backend-functions.js";
+import { getAllUsers } from "../common/backend-functions.js";
 
-document.addEventListener('DOMContentLoaded', async function() {
-    var createAccountButton = document.querySelector(".create-account-button");
+document.addEventListener('DOMContentLoaded', function() {
+    var loginButton = document.querySelector(".login-button");
+    
+    loginButton.addEventListener('click', async function() {
+        var email = document.getElementById('email-input').value.toString().trim();
+        var password = document.getElementById('password-input').value.toString().trim();
+        
+        try {
+            const users = await getAllUsers();
 
-    
-    
-    createAccountButton.addEventListener('click', signup(){
-        var nickname = document.getElementById("nickname-data");
-        var email = document.getElementById('email-data');
-        var password =  document.getElementById('password-data');
-        var description = "jfkdjflksd";
-        var image = "dkjdksjf";
-        setUserData(email, nickname, description, image, password);
+            let loggedIn = false;
+            for (const user of Object.values(users)) {
+                if (user.email === email && user.password === password) {
+                    loggedIn = true;
+                    event.preventDefault();
+                    sessionStorage.setItem("actualUser", user.username);
+                    sessionStorage.setItem("userMail", user.email);
+                    window.location.href = '../../src/login/user-profile.html';
+                    alert("Inicio de sesión correcto");
+                    break;
+                }
+            }
+
+            if (!loggedIn) {
+                alert("Correo electrónico o contraseña incorrectos");
+            }
+        } catch (error) {
+            alert("Error al obtener todos los usuarios:", error);
+        }
     });
-
 });
