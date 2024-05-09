f7446e2bb2c090ca769b979b9c9da78a8232f0e7
Mauro Gómez <146375895+MGGdesigns@users.noreply.github.com>
2024-05-09 21:55:12 +0100

    Visual changes to waiting lobby and loader fixes

---
+++ WizQuizz/js-files/play/waiting-lobby.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () =>{
+        document.body.removeChild(loader);
+    })
+})
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
